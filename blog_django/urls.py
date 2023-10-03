0cba68f1483ef21c286dbd705db8ec91622abde0
Cloyd Balongkit <cloydbalongkit@Cloyds-MacBook-Pro.local>
2023-10-03 22:28:33 +0800

    Adding Login and Validations

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from users import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     path("users/", include("users.urls")),
+    path("profile/", views.profile, name='profile'),
+    path("login/", auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path("logout/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
